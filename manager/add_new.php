352be1e0fd94cb1eb55673bfab2ee14215e24f3f
Seth Shoultes <seth@eventespresso.com>
2011-11-12 04:02:17 +0000

    Added the help boxes the ticket editor and added the custom ticket tags. [touch:189]

--- manager/add_new.php
+++ manager/add_new.php
@@ -6,37 +6,36 @@ function add_new_event_ticket(){
 <!--Add event display-->
 
 <div class="metabox-holder">
-  <div class="postbox">
-    <h3>
-      <?php _e('Add a Ticket Template','event_espresso'); ?>
-    </h3>
-    <div class="inside">
-      <form id="add-edit-new-event-ticket" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
-        <input type="hidden" name="action" value="add">
-        <ul>
-          <li>
-            <label for="ticket_name">
-              <?php _e('Ticket Name','event_espresso'); ?>
-            </label>
-            <input type="text" name="ticket_name" size="25" />
-							<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_name_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
-          </li>
-          <li>
-            <label for="base-ticket-select" <?php echo $styled ?>>
-              <?php _e('Select Base Template', 'event_espresso');  ?>
-            </label>
-            <select id="base-ticket-select" class="wide" <?php echo $disabled ?> name="ticket_file">
-              <option <?php espresso_ticket_is_selected($fname) ?> value="basic.html">
-              <?php _e('Default Template - Basic', 'event_espresso'); ?>
-              </option>
-              <?php foreach( $files as $fname ) { ?>
-              <option <?php espresso_ticket_is_selected($fname) ?> value="<?php echo $fname ?>"><?php echo $fname; ?></option>
-              <?php } ?>
-            </select> <a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=base_template_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
-          </li>
-          <li>
-            <div id="ticket-logo-image">
-              <?php
+	<div class="postbox">
+		<h3>
+			<?php _e('Add a Ticket Template','event_espresso'); ?>
+		</h3>
+		<div class="inside">
+			<form id="add-edit-new-event-ticket" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
+				<input type="hidden" name="action" value="add">
+				<ul>
+					<li>
+						<label for="ticket_name">
+							<?php _e('Ticket Name','event_espresso'); ?>
+						</label>
+						<input type="text" name="ticket_name" size="25" />
+						<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_name_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a> </li>
+					<li>
+						<label for="base-ticket-select" <?php echo $styled ?>>
+							<?php _e('Select Base Template', 'event_espresso');  ?>
+						</label>
+						<select id="base-ticket-select" class="wide" <?php echo $disabled ?> name="ticket_file">
+							<option <?php espresso_ticket_is_selected($fname) ?> value="basic.html">
+							<?php _e('Default Template - Basic', 'event_espresso'); ?>
+							</option>
+							<?php foreach( $files as $fname ) { ?>
+							<option <?php espresso_ticket_is_selected($fname) ?> value="<?php echo $fname ?>"><?php echo $fname; ?></option>
+							<?php } ?>
+						</select>
+						<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=base_template_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a> </li>
+					<li>
+						<div id="ticket-logo-image">
+							<?php
 
 		if(!empty($ticket_logo_url)){
 			$ticket_logo = $ticket_logo_url;
@@ -44,51 +43,50 @@ function add_new_event_ticket(){
 			$ticket_logo = '';
 		}
 		?>
-              <?php // var_dump($event_meta['event_thumbnail_url']); ?>
-              <label for="upload_image">
-                <?php _e('Add a Logo', 'event_espresso'); ?>
-              </label>
-              <input id="upload_image" type="hidden" size="36" name="upload_image" value="<?php echo $ticket_logo ?>" />
-              <input id="upload_image_button" type="button" value="Upload Image" /> <a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_logo_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
-              <?php if($ticket_logo){ ?>
-              <p class="ticket-logo"><img src="<?php echo $ticket_logo ?>" alt="" /></p>
-              <a id='remove-image' href='#' title='<?php _e('Remove this image', 'event_espresso'); ?>' onclick='return false;'>
-              <?php _e('Remove Image', 'event_espresso'); ?>
-              </a>
-              <?php } ?>
-            </div>
-          </li>
-          <li>
-            <div id="descriptiondivrich" class="postarea">
-              <label for="ticket_content">
-                <?php _e('Ticket Description/Instructions','event_espresso'); ?> <a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_description_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
-              </label>
-              <div class="postbox">
-                <?php the_editor('', $id = 'ticket_content', $prev_id = 'title', $media_buttons = true, $tab_index = 3);?>
-                <table id="manage-event-ticket-form" cellspacing="0">
-                  <tbody>
-                    <tr>
-                      <td class="aer-word-count"></td>
-                      <td class="autosave-info"><span> <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=custom_ticket_info">
-                        <?php _e('View Custom Ticket Tags', 'event_espresso'); ?>
-                        </a> | <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=custom_ticket_example">
-                        <?php _e('Ticket Example','event_espresso'); ?>
-                        </a> </span></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </li>
-          <li>
-            <p>
-              <input class="button-primary" type="submit" name="Submit" value="<?php _e('Add Ticket'); ?>" id="add_new_ticket" />
-            </p>
-          </li>
-        </ul>
-      </form>
-    </div>
-  </div>
+							<?php // var_dump($event_meta['event_thumbnail_url']); ?>
+							<label for="upload_image">
+								<?php _e('Add a Logo', 'event_espresso'); ?>
+							</label>
+							<input id="upload_image" type="hidden" size="36" name="upload_image" value="<?php echo $ticket_logo ?>" />
+							<input id="upload_image_button" type="button" value="Upload Image" />
+							<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_logo_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
+							<?php if($ticket_logo){ ?>
+							<p class="ticket-logo"><img src="<?php echo $ticket_logo ?>" alt="" /></p>
+							<a id='remove-image' href='#' title='<?php _e('Remove this image', 'event_espresso'); ?>' onclick='return false;'>
+							<?php _e('Remove Image', 'event_espresso'); ?>
+							</a>
+							<?php } ?>
+						</div>
+					</li>
+					<li>
+						<div id="descriptiondivrich" class="postarea">
+							<label for="ticket_content">
+								<?php _e('Ticket Description/Instructions','event_espresso'); ?>
+								<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_description_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a> </label>
+							<div class="postbox">
+								<?php the_editor('', $id = 'ticket_content', $prev_id = 'title', $media_buttons = true, $tab_index = 3);?>
+								<table id="manage-event-ticket-form" cellspacing="0">
+									<tbody>
+										<tr>
+											<td class="aer-word-count"></td>
+											<td class="autosave-info"><span> <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=custom_ticket_tags">
+												<?php _e('View Custom Ticket Tags', 'event_espresso'); ?>
+												</a></span></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</li>
+					<li>
+						<p>
+							<input class="button-primary" type="submit" name="Submit" value="<?php _e('Add Ticket'); ?>" id="add_new_ticket" />
+						</p>
+					</li>
+				</ul>
+			</form>
+		</div>
+	</div>
 </div>
 <script type="text/javascript" charset="utf-8">
 	//<![CDATA[
@@ -127,3 +125,4 @@ function add_new_event_ticket(){
 <?php
 //espresso_tiny_mce();
 }
+
--- manager/edit_ticket.php
+++ manager/edit_ticket.php
@@ -14,35 +14,38 @@ function edit_event_ticket(){
 	?>
 
 <div class="metabox-holder">
-  <div class="postbox">
-    <h3>
-      <?php _e('Edit Ticket Template:','event_espresso'); ?>
-      <?php echo stripslashes($ticket_name) ?></h3>
-    <div class="inside">
-      <form id="add-edit-new-event-ticket" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
-        <input type="hidden" name="ticket_id" value="<?php echo $ticket_id; ?>">
-        <input type="hidden" name="action" value="update_ticket">
-        <ul>
-          <li>
-            <label>
-              <?php _e('Ticket Name:','event_espresso'); ?>
-            </label>
-            <input type="text" name="ticket_name" size="25" value="<?php echo stripslashes($ticket_name);?>" />
-          </li>
-          <li>
-            <label for="base-ticket-select" <?php echo $styled ?>>
-              <?php _e('Select Base Template', 'event_espresso');  ?>
-            </label>
-            <select id="base-ticket-select" class="wide" <?php echo $disabled ?> name="ticket_file">
-              <option <?php espresso_ticket_is_selected($fname,$ticket_file) ?> value="basic.html">
-              <?php _e('Default Template - Basic', 'event_espresso'); ?>
-              </option>
-              <?php foreach( $files as $fname ) { ?>
-              <option <?php espresso_ticket_is_selected($fname,$ticket_file) ?> value="<?php echo $fname ?>"><?php echo $fname; ?></option>
-              <?php } ?>
-            </select>
-          </li><li><div id="ticket-logo-image">
-		  <?php
+	<div class="postbox">
+		<h3>
+			<?php _e('Edit Ticket Template:','event_espresso'); ?>
+			<?php echo stripslashes($ticket_name) ?></h3>
+		<div class="inside">
+			<form id="add-edit-new-event-ticket" method="post" action="<?php echo $_SERVER['REQUEST_URI'];?>">
+				<input type="hidden" name="ticket_id" value="<?php echo $ticket_id; ?>">
+				<input type="hidden" name="action" value="update_ticket">
+				<ul>
+					<li>
+						<label>
+							<?php _e('Ticket Name:','event_espresso'); ?>
+						</label>
+						<input type="text" name="ticket_name" size="25" value="<?php echo stripslashes($ticket_name);?>" />
+						<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_name_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a> </li>
+					<li>
+						<label for="base-ticket-select" <?php echo $styled ?>>
+							<?php _e('Select Base Template', 'event_espresso');  ?>
+						</label>
+						<select id="base-ticket-select" class="wide" <?php echo $disabled ?> name="ticket_file">
+							<option <?php espresso_ticket_is_selected($fname,$ticket_file) ?> value="basic.html">
+							<?php _e('Default Template - Basic', 'event_espresso'); ?>
+							</option>
+							<?php foreach( $files as $fname ) { ?>
+							<option <?php espresso_ticket_is_selected($fname,$ticket_file) ?> value="<?php echo $fname ?>"><?php echo $fname; ?></option>
+							<?php } ?>
+						</select>
+						<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=base_template_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
+					</li>
+					<li>
+						<div id="ticket-logo-image">
+							<?php
 									 
 		if(!empty($ticket_logo_url)){ 
 			$ticket_logo = $ticket_logo_url;
@@ -50,50 +53,53 @@ function edit_event_ticket(){
 			$ticket_logo = '';
 		}
 		?>
-		  <?php // var_dump($event_meta['event_thumbnail_url']); ?>
-		  <label for="upload_image">
-			<?php _e('Add a Logo', 'event_espresso'); ?>
-		  </label>
-		  <input id="upload_image" type="hidden" size="36" name="upload_image" value="<?php echo $ticket_logo ?>" />
-		  <input id="upload_image_button" type="button" value="Upload Image" />
-		  <?php if($ticket_logo){ ?>
-		  <p class="ticket-logo-thumb"><img src="<?php echo $ticket_logo ?>" alt="" /></p>
-          <a id='remove-image' href='#' title='<?php _e('Remove this image', 'event_espresso'); ?>' onclick='return false;'><?php _e('Remove Image', 'event_espresso'); ?></a>
-		  <?php } ?>
-		</div></li>
-          <li>
-            <div id="descriptiondivrich" class="postarea">
-              <label for="ticket_content">
-                <?php _e('Ticket Description/Instructions','event_espresso'); ?>
-              </label>
-              <div class="postbox">
-                <?php the_editor(stripslashes_deep($ticket_content), $id = 'ticket_content', $prev_id = 'title', $media_buttons = true, $tab_index = 3);?>
-                <table id="manage-event-ticket-form" cellspacing="0">
-                  <tbody>
-                    <tr>
-                      <td class="aer-word-count"></td>
-                      <td class="autosave-info"><span> <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=custom_ticket_info">
-                        <?php _e('View Custom Ticket Tags', 'event_espresso'); ?>
-                        </a> | <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=custom_ticket_example">
-                        <?php _e('Ticket Example','event_espresso'); ?></a> 
-                        | <a class="thickbox" href="<?php echo ESPRESSO_TICKETING_FULL_URL.'templates/'.$ticket_file; ?>?TB_iframe=true&height=200&width=630">
-                        <?php _e('Preview','event_espresso'); ?>
-                        </a> </span></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </li>
-          <li>
-            <p>
-              <input class="button-primary" type="submit" name="Submit" value="<?php _e('Update Ticket'); ?>" id="update_ticket" />
-            </p>
-          </li>
-        </ul>
-      </form>
-    </div>
-  </div>
+							<?php // var_dump($event_meta['event_thumbnail_url']); ?>
+							<label for="upload_image">
+								<?php _e('Add a Logo', 'event_espresso'); ?>
+							</label>
+							<input id="upload_image" type="hidden" size="36" name="upload_image" value="<?php echo $ticket_logo ?>" />
+							<input id="upload_image_button" type="button" value="Upload Image" />
+							<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_logo_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
+							<?php if($ticket_logo){ ?>
+							<p class="ticket-logo-thumb"><img src="<?php echo $ticket_logo ?>" alt="" /></p>
+							<a id='remove-image' href='#' title='<?php _e('Remove this image', 'event_espresso'); ?>' onclick='return false;'>
+							<?php _e('Remove Image', 'event_espresso'); ?>
+							</a>
+							<?php } ?>
+						</div>
+					</li>
+					<li>
+						<div id="descriptiondivrich" class="postarea">
+							<label for="ticket_content">
+								<?php _e('Ticket Description/Instructions','event_espresso'); ?>
+								<a class="thickbox"  href="#TB_inline?height=400&amp;width=500&amp;inlineId=ticket_description_info" target="_blank"><img src="<?php echo EVENT_ESPRESSO_PLUGINFULLURL ?>images/question-frame.png" width="16" height="16" alt="" /></a>
+							</label>
+							<div class="postbox">
+								<?php the_editor(stripslashes_deep($ticket_content), $id = 'ticket_content', $prev_id = 'title', $media_buttons = true, $tab_index = 3);?>
+								<table id="manage-event-ticket-form" cellspacing="0">
+									<tbody>
+										<tr>
+											<td class="aer-word-count"></td>
+											<td class="autosave-info"><span> <a class="thickbox" href="#TB_inline?height=300&width=400&inlineId=custom_ticket_tags">
+												<?php _e('View Custom Ticket Tags', 'event_espresso'); ?>
+												</a> | <a class="thickbox" href="<?php echo ESPRESSO_TICKETING_FULL_URL.'templates/'.$ticket_file; ?>?TB_iframe=true&height=200&width=630">
+												<?php _e('Preview','event_espresso'); ?>
+												</a> </span></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</li>
+					<li>
+						<p>
+							<input class="button-primary" type="submit" name="Submit" value="<?php _e('Update Ticket'); ?>" id="update_ticket" />
+						</p>
+					</li>
+				</ul>
+			</form>
+		</div>
+	</div>
 </div>
 <script type="text/javascript" charset="utf-8">
 	//<![CDATA[
@@ -137,3 +143,4 @@ function edit_event_ticket(){
 <?php 
  //espresso_tiny_mce();
 }
+
