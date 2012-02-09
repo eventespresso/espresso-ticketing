f1ab6a5c775c9c037c8900ff3dfa9374298a2cf3
Seth Shoultes <seth@eventespresso.com>
2012-02-09 21:11:40 +0000

    Changing "class" folder name to "tpc". [touch:719]

--- functions.php
+++ functions.php
@@ -153,7 +153,7 @@ function espresso_ticket_launch($attendee_id=0, $registration_id=0){
 	if ( (isset($_REQUEST['pdf']) && $_REQUEST['pdf']==true)){
 		//Create the PDF
 		define('DOMPDF_ENABLE_REMOTE',true);
-		require_once(EVENT_ESPRESSO_PLUGINFULLPATH . '/class/dompdf/dompdf_config.inc.php');
+		require_once(EVENT_ESPRESSO_PLUGINFULLPATH . '/tpc/dompdf/dompdf_config.inc.php');
 		$dompdf = new DOMPDF();
 		$dompdf->load_html($content);
 		//$dompdf->set_paper('A4', 'landscape');
