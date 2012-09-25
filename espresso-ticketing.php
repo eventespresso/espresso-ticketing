deab4335fb38d57008c1a9c63b70cbd8b026c3d8
Chris Reynolds <chris@eventespresso.com>
2012-09-25 18:19:21 +0000

    fixed version

--- espresso-ticketing.php
+++ espresso-ticketing.php
@@ -4,7 +4,7 @@
   Plugin URI: http://eventespresso.com/
   Description: Ticketing system for Event Espresso
 
-  Version: 2.0.8-beta
+  Version: 2.0.7-beta
 
   Author: Seth Shoultes
   Author URI: http://www.eventespresso.com
